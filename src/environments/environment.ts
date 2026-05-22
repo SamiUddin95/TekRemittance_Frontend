b1bab8e092032beddccd76f288537d9cc5539f3c
teknoloje <teknoloje@teknoloje.com>
2026-05-22 16:22:13 +0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://localhost:44367/api',
+  //apiUrl: 'https://localhost:44367/api',
+  apiUrl: 'http://10.0.0.1:8097/api',
   ssrsUrl: 'http://localhost/Reports',
   //apiUrl: 'https://localhost:44367/api',
   //apiUrl: 'http://175.107.195.221:8501/api',
